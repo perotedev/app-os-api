ab670b6bbde667450225ae4551c480399712c443
Rodrigo Perote <rodripema@hotmail.com>
2025-10-01 21:53:45 -0400

    core: db config

--- app/core/config.py
+++ app/core/config.py
@@ -2,21 +2,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "FastAPI Project"
+    PROJECT_NAME: str = "App OS API"
     API_V1_STR: str = "/api/v1"
 
-    POSTGRES_SERVER: str = "localhost"
-    POSTGRES_USER: str = "user"
-    POSTGRES_PASSWORD: str = "password"
-    POSTGRES_DB: str = "fastapi_db"
-    POSTGRES_PORT: str = "5432"
-    DATABASE_URL: str = (
-        f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
-        f"@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
-    )
+    POSTGRES_SERVER: str = ""
+    POSTGRES_USER: str = ""
+    POSTGRES_PASSWORD: str = ""
+    POSTGRES_DB: str = ""
+    POSTGRES_PORT: str = ""
+    DB_CONNECTION_DRIVER: str =""
+    DATABASE_URL: str = ""
 
     APPLICATION_PORT: int = 8001
-    SECRET_KEY: str = "super-secret-key"
+    SECRET_KEY: str = ""
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
@@ -24,3 +22,5 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
+
+settings.DATABASE_URL = f"{settings.DB_CONNECTION_DRIVER}://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
