cb1881be78a61946624c1c1c17cefb7427639f2d
Rodrigo Perote <rodripema@hotmail.com>
2025-11-03 02:20:31 -0400

    refactor: remove address update logic from CRUDClient update method

--- app/crud/crud_client.py
+++ app/crud/crud_client.py
@@ -26,11 +26,7 @@ class CRUDClient(CRUDBase[Client, ClientCreate, ClientUpdate]):
             update_data = obj_in
         else:
             update_data = obj_in.model_dump(exclude_unset=True)
-        
-        if "address" in update_data and update_data["address"] is not None:
-            address_update_data = update_data.pop("address")
-            crud_address.update(db, db_obj=db_obj.address, obj_in=address_update_data)
-
+            
         return super().update(db, db_obj=db_obj, obj_in=update_data)
 
 client = CRUDClient(Client)
